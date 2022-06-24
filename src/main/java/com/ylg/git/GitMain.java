d2edfb300b9658341a8ffda86e0be66862f8c80b
yanglingguang <2631012464@qq.com>
2022-06-24 20:40:39 +0800

    third commit

--- src/main/java/com/ylg/git/GitMain.java
+++ src/main/java/com/ylg/git/GitMain.java
@@ -2,7 +2,8 @@ package com.ylg.git;
 
 public class GitMain {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
