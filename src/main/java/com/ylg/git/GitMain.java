a0d6a63f9f1b1f46067f7169fee8226ec040e85c
yanglingguang <2631012464@qq.com>
2022-06-24 21:02:14 +0800

    hot-fix commit

--- src/main/java/com/ylg/git/GitMain.java
+++ src/main/java/com/ylg/git/GitMain.java
@@ -5,5 +5,6 @@ public class GitMain {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello hot-fix");
     }
 }
