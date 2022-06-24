a11ff6e35139af1e1b1bd4beda9b0b3ef15f7fb3
yanglingguang <2631012464@qq.com>
2022-06-24 21:41:20 +0800

    master commit2

--- src/main/java/com/ylg/git/GitMain.java
+++ src/main/java/com/ylg/git/GitMain.java
@@ -8,5 +8,6 @@ public class GitMain {
         System.out.println("hello hot-fix");
         System.out.println("hello master");
         System.out.println("hello hot-fix test");
+        System.out.println("push test");
     }
 }
