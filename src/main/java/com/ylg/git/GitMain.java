40c45e591db0a79c9cccc4b083901c30c0ccc3af
yanglingguang <2631012464@qq.com>
2022-06-24 20:39:47 +0800

    second commit

--- src/main/java/com/ylg/git/GitMain.java
+++ src/main/java/com/ylg/git/GitMain.java
@@ -3,5 +3,6 @@ package com.ylg.git;
 public class GitMain {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
