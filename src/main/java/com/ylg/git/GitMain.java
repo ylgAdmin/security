7865cdb7af3afc7d773ebe7daa9343f265b4a9c0
ylgAdmin <70802878+ylgAdmin@users.noreply.github.com>
2022-06-24 21:59:09 +0800

    pull commit

--- src/main/java/com/ylg/git/GitMain.java
+++ src/main/java/com/ylg/git/GitMain.java
@@ -9,5 +9,6 @@ public class GitMain {
         System.out.println("hello master");
         System.out.println("hello hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
